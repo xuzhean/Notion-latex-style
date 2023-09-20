7d12154f812c4cf27b27ee37a2fc060803f4e99d
Xu Zhean <94977922+xuzhean@users.noreply.github.com>
2023-09-20 16:56:26 +0800

    Update notion-latex-sytle.js

--- notion-latex-sytle.js
+++ notion-latex-sytle.js
@@ -28,13 +28,25 @@
 	  .notion-page-content {
           font-family: KaTeX_Main, Georgia, ui-serif, serif !important;
 		  line-height: 1.35em !important;
+		  padding-left: calc(32px + env(safe-area-inset-left)) !important;
+          padding-right: calc(32px + env(safe-area-inset-right)) !important;
 	  }
 	  .notranslate {
 		  padding: 2px 2px !important;
+	      text-align: justify !important;
 	  }
 	  .pseudoSelection {
           min-height: calc(1.35em + 4px) !important;
 	  }
+      .notion-selectable {
+          max-width: none !important;
+      }
+	  .katex-display {
+		  margin: 0 !important;	
+	  }
+	  [role=dialog] {
+		  width: 640px !important;
+	  }
     `
-    GM_addStyle(css)
+    GM_addStyle(css);
 })();
